537137c4d9e1f31a8d7f5db85efa5ef9a58ca492
pancho horrillo <pancho@pancho.name>
2021-10-05 14:28:31 +0200

    Select IBM Plex as font

--- config.el
+++ config.el
@@ -21,6 +21,9 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "IBM Plex Mono" :size 18)
+      doom-big-font (font-spec :family "IBM Plex Mono" :size 32)
+      doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 18))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
