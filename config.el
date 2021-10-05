d209ec76ed792cbef85f58c3f4da3a639dba4196
pancho horrillo <pancho@pancho.name>
2021-10-05 14:19:43 +0200

    Use doom-colors theme for treemacs (with icons)

--- config.el
+++ config.el
@@ -27,6 +27,10 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-dracula)
 
+;; Use `doom-colors' theme on treemacs (with icons!)
+;; Possible caveat: https://github.com/hlissner/emacs-doom-themes/issues/392
+(setq doom-themes-treemacs-theme 'doom-colors)
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
