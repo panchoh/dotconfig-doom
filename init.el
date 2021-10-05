a2dda0d10b8f75b33c94a97f4bddbcbd2cd7bcc0
pancho horrillo <pancho@pancho.name>
2021-10-05 13:15:43 +0200

    Enable :lang (json +lsp)

--- init.el
+++ init.el
@@ -135,7 +135,7 @@
        (haskell +lsp)        ; a language that's lazier than I am
        ;; hy                 ; readability of scheme w/ speed of python
        ;; idris              ; a language you can depend on
-       ;; json               ; At least it ain't XML
+       (json +lsp)           ; At least it ain't XML
        ;; (java +meghanada)  ; the poster child for carpal tunnel syndrome
        ;; javascript         ; all(hope(abandon(ye(who(enter(here))))))
        ;; julia              ; a better, faster MATLAB
