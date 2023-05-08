66856f884eebb716f2bb87fa69d932826682acd8
pancho horrillo <pancho@pancho.name>
2023-05-08 13:39:49 +0200

    feat(init): tweak python config

--- init.el
+++ init.el
@@ -160,7 +160,10 @@
        ;; php                ; perl's insecure younger brother
        ;; plantuml           ; diagrams for confusing people more
        ;; purescript         ; javascript, but functional
-       (python +lsp +pyenv)  ; beautiful is better than ugly
+       (python
+        +lsp
+        +pyenv
+        +treesitter)  ; beautiful is better than ugly
        ;; qt                 ; the 'cutest' gui framework ever
        ;; racket             ; a DSL for DSLs
        ;; raku               ; the artist formerly known as perl6
