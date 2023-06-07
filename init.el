34a66251bd0f25541f402d6077ee0269a72b7229
pancho horrillo <pancho@pancho.name>
2023-06-07 13:10:10 +0200

    Disable indent-guides, they break mini-map

--- init.el
+++ init.el
@@ -40,7 +40,7 @@
         +unicode)            ; 🙂
        hl-todo               ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;; hydra
-       indent-guides         ; highlighted indent columns
+       ;; indent-guides      ; highlighted indent columns
        (ligatures
         +extra)              ; ligatures and symbols to make your code pretty again
        minimap               ; show a map of the code on the side
