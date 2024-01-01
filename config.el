bb8de37754e1f2c46e6492acd20edc901a92cdbd
pancho horrillo <pancho@pancho.name>
2024-01-01 13:13:02 +0100

    refactor(config): adjust font sizes

--- config.el
+++ config.el
@@ -21,11 +21,11 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font                 (font-spec :family "Iosevka"          :size 22 :weight 'light)
+(setq doom-font                 (font-spec :family "Iosevka"          :size 18 :weight 'light)
       doom-big-font             (font-spec :family "Iosevka"          :size 32 :weight 'light)
-      doom-symbol-font          (font-spec :family "Noto Color Emoji" :size 22 :weight 'light)
-      doom-variable-pitch-font  (font-spec :family "Iosevka Aile"     :size 20 :weight 'light)
-      doom-serif-font           (font-spec :family "Iosevka Slab"     :size 22 :weight 'light))
+      doom-symbol-font          (font-spec :family "Noto Color Emoji" :size 18 :weight 'light)
+      doom-variable-pitch-font  (font-spec :family "Iosevka Aile"     :size 16 :weight 'light)
+      doom-serif-font           (font-spec :family "Iosevka Slab"     :size 18 :weight 'light))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
