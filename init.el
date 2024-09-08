3ec6f6418ad8b533fb6e5d49e0ce55c963b66a45
pancho horrillo <pancho@pancho.name>
2024-09-08 09:54:27 +0200

    feat(init): enable pass

--- init.el
+++ init.el
@@ -128,7 +128,8 @@
        (magit
         +forge)              ; a git porcelain for Emacs
        make                  ; run make tasks from Emacs
-       ;; pass               ; password manager for nerds
+       (pass
+        +auth)               ; password manager for nerds
        pdf                   ; pdf enhancements
        ;; prodigy            ; FIXME managing external services & code builders
        ;; rgb                ; creating color strings
