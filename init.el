c10ec267b16ea9ea2a67cff5f19158b73e90bea5
pancho horrillo <pancho@pancho.name>
2025-04-02 09:13:36 +0200

    chore(init): drop prodigy
    Unavailable since https://github.com/doomemacs/doomemacs/commit/f9e9c27a8b6d79e109a1106bf0412979c90cd006

--- init.el
+++ init.el
@@ -135,7 +135,6 @@
        (pass
         +auth)               ; password manager for nerds
        pdf                   ; pdf enhancements
-       ;; prodigy            ; FIXME managing external services & code builders
        ;; rgb                ; creating color strings
        ;; taskrunner         ; taskrunner for all your projects
        terraform             ; infrastructure as code
